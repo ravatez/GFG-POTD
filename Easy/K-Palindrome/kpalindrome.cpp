94ad8f81363ec79f6095b2d77b2f1f53706bff46
Rupesh Ravate <ravatez@gmail.com>
2024-05-23 18:52:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/K-Palindrome/kpalindrome.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int lcs(string& s, string& r, int i, int j, vector<vector<int>>&dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s[i]==r[j]){
+            return dp[i][j]=1+lcs(s,r, i-1, j-1, dp);
+        }
+        
+        return dp[i][j]=max(lcs(s, r, i-1,j,dp), lcs(s,r,i,j-1,dp));
+    }
+
+    int kPalindrome(string str, int n, int k)
+    {
+        // code here
+        string r=str;
+        reverse(r.begin(), r.end());
+        
+        vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+        
+        int x=lcs(str, r, n-1, n-1, dp);
+        
+        int rem=n-x;
+        return rem<=k;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.kPalindrome(str, n, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
