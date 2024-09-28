df5d80095007462402341a71cce0398e89af4790
Rupesh Ravate <ravatez@gmail.com>
2024-09-28 17:25:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.py
@@ -0,0 +1,33 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    def minimizeCost(self, k, arr):
+        # code here
+        n=len(arr)
+        dp=[float ('inf')]*n
+        dp[0]=0
+        for i in range(1,n):
+            for j in range(max(0,i-k),i):
+                dp[i]=min(dp[i],dp[j]+abs(arr[i]-arr[j]))
+        return dp[n-1]
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        k= int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.minimizeCost(k,arr)
+        print(res)
+        t -= 1
+
+
+# } Driver Code Ends
