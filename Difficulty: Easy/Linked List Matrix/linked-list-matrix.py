bc456fb906b1f9a771abb990a2d189ea79dcb1fe
Rupesh Ravate <ravatez@gmail.com>
2024-10-09 18:17:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Matrix/linked-list-matrix.py
@@ -0,0 +1,85 @@
+#User function Template for python3
+
+'''
+
+class Node():
+    def __init__(self,x):
+        self.data = x
+        self.right = None
+        self.down=None
+
+'''
+
+class Solution:
+    def constructLinkedMatrix(self, mat):
+        #your code goes here
+        flag=0
+        n=len(mat)
+        for i in range(n):
+            f1=0
+            for j in range(n):
+                if not flag:
+                    head=Node(mat[i][j])
+                    flag=1
+                    cur=head
+                if i+1<n:
+                    cur.down=Node(mat[i+1][j])
+                if j+1<n:
+                    cur.right=Node(mat[i][j+1])
+                if not f1:
+                    temp=cur
+                    f1=1
+                cur=cur.right
+            cur=temp.down
+        return head
+
+#{ 
+ # Driver Code Starts
+class Node():
+
+    def __init__(self, x):
+        self.data = x
+        self.right = None
+        self.down = None
+
+
+def display(head):
+    Dp = head
+    while Dp:
+        Rp = Dp
+        while Rp:
+            print(Rp.data, end=" ")
+            if Rp.right:
+                print(Rp.right.data, end=" ")
+            else:
+                print("null", end=" ")
+            if Rp.down:
+                print(Rp.down.data, end=" ")
+            else:
+                print("null", end=" ")
+            Rp = Rp.right
+        Dp = Dp.down
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        # First row input
+        a = list(map(int, input().strip().split()))
+        n = len(a)
+
+        # Input the matrix
+        mat = [a]
+        for i in range(1, n):
+            row = list(map(int, input().strip().split()))
+            mat.append(row)
+
+        # Create a Solution object and construct the linked matrix
+        obj = Solution()
+        head = obj.constructLinkedMatrix(mat)
+        if head is None:
+            print(-1)
+            continue
+        display(head)
+        print()
+
+# } Driver Code Ends
