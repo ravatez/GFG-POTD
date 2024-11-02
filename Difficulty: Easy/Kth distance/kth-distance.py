769344b7b3a8a04d30107ea8d96c29d67ff5bc04
Rupesh Ravate <ravatez@gmail.com>
2024-11-02 08:03:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+    def checkDuplicatesWithinK(self, arr, k):
+        # your code
+        for i in range(len(arr)-1):
+            if(arr[i] in arr[i+1:i+k+1]):
+                return True
+        return False
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+# Position this line where user code will be pasted.
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        k = int(input())
+        ob = Solution()
+        res = ob.checkDuplicatesWithinK(arr, k)
+        if res:
+            print("true")
+        else:
+            print("false")
+        # print(res)
+        print("~")
+        t -= 1
+# } Driver Code Ends
