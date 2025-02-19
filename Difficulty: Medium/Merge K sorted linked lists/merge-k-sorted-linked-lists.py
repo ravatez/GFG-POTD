d7dec6036d01ea052cc0e21e14505af785cedc0c
Rupesh Ravate <ravatez@gmail.com>
2025-02-19 16:58:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.py
@@ -0,0 +1,80 @@
+#User function Template for python3
+'''
+class Node:
+    def _init_(self,x):
+        self.data = x
+        self.next = None
+'''
+class Solution:
+    def mergeKLists(self, arr):
+        # code here
+        # return head of merged list
+        n=len(arr)
+        arry=[]
+        for i in range(n):
+            curr=arr[i]
+            while curr:
+                arry.append(curr.data)
+                curr = curr.next
+        arry.sort()
+        n1=len(arry)
+        head=Node(arry[0])
+        curr=head
+        for i in range(1,n1):
+            curr.next=Node(arry[i])
+            curr=curr.next
+        
+        return head
+
+#{ 
+ # Driver Code Starts
+import heapq
+
+
+class Node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+    # To compare nodes in the heap
+    def __lt__(self, other):
+        return self.data < other.data
+
+
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        lists = []
+        for _ in range(n):
+            values = list(map(int, input().split()))
+            head = None
+            temp = None
+            for value in values:
+                newNode = Node(value)
+                if head is None:
+                    head = newNode
+                    temp = head
+                else:
+                    temp.next = newNode
+                    temp = temp.next
+            lists.append(head)
+
+        sol = Solution()
+        head = sol.mergeKLists(lists)
+        printList(head)
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
