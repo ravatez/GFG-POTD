d857aeef6dc883e1278bef25a8cbb2d9ed74783f
Rupesh Ravate <ravatez@gmail.com>
2025-03-29 21:08:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.py
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.py
@@ -1,80 +1,41 @@
-#User function Template for python3
-'''
-class Job:
-    
-    # Job class which stores profit and deadline.
-    
-    def __init__(self,profit=0,deadline=0):
-        self.profit = profit
-        self.deadline = deadline
-        self.id = 0
-'''        
-
 class Solution:
-    
-    #Function to find the maximum profit and the number of jobs done.
-    def JobScheduling(self,Jobs,n):
-        
+    def jobSequencing(self, deadline, profit):
         # code here
-                
-        # Sorting jobs by decreasing order of profit
-        Jobs.sort(key=lambda x: x.profit, reverse=True)
-        
-        # Finding the maximum deadline to create a timeline
-        max_deadline = max(job.deadline for job in Jobs)
-        
-        # Timeline to keep track of free slots (initialized to None)
-        timeline = [None] * (max_deadline + 1)
-        
-        job_count = 0
+        from heapq import heappush, heappop
+        jobs = list(zip(deadline, profit))
+        jobs.sort()
         max_profit = 0
+        job_count = 0
+        max_heap = []
+        index = len(jobs) - 1
         
-        # Iterating over each job
-        for job in Jobs:
-            # Try to find a slot from job.deadline to 1
-            for t in range(job.deadline, 0, -1):
-                if timeline[t] is None:
-                    # Slot found, schedule the job
-                    timeline[t] = job.id
-                    job_count += 1
-                    max_profit += job.profit
-                    break
+        for i in range(jobs[-1][0], 0, -1):
+            while index >= 0 and jobs[index][0] == i:
+                heappush(max_heap, -jobs[index][1]) 
+                index -= 1
+            
+            if max_heap:
+                job_count += 1
+                max_profit += -heappop(max_heap)
         
-        return job_count, max_profit
+        return [job_count, max_profit]
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
-import atexit
-import io
-import sys
+import heapq
 
+#Position this line where user code will be pasted.
 
-#Contributed by : Nagendra Jha
-class Job:
-    '''
-    Job class which stores profit and deadline.
-    '''
+if __name__ == "__main__":
+    t = int(input().strip())
 
-    def __init__(self, profit=0, deadline=0):
-        self.profit = profit
-        self.deadline = deadline
-        self.id = 0
+    for _ in range(t):
+        deadline = list(map(int, input().strip().split()))
+        profit = list(map(int, input().strip().split()))
 
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        n = int(input())
-        info = list(map(int, input().strip().split()))
-        Jobs = [Job() for i in range(n)]
-        for i in range(n):
-            Jobs[i].id = info[3 * i]
-            Jobs[i].deadline = info[3 * i + 1]
-            Jobs[i].profit = info[3 * i + 2]
-        ob = Solution()
-        res = ob.JobScheduling(Jobs, n)
-        print(res[0], end=" ")
-        print(res[1])
-
+        obj = Solution()
+        ans = obj.jobSequencing(deadline, profit)
+        print(ans[0], ans[1])
+        print("~")
 # } Driver Code Ends
