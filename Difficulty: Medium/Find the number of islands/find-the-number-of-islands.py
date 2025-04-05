01213b2df6a1cec60a724e4e284b07c5f8e02817
Rupesh Ravate <ravatez@gmail.com>
2025-04-05 18:21:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.py
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.py
@@ -1,33 +1,48 @@
 #User function Template for python3
+
 class Solution:
+    def isSafe(self, i, j, n, m):
+        return (0 <= i < n) and (0 <= j < m)
+        
+    def dfs(self, p, vis, n, m):
+        dirs = ((0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1))
+        for dx, dy in dirs:
+            x, y = p[0] + dx, p[1] + dy
+            if self.isSafe(x, y, n, m) and not vis[x][y]:
+                vis[x][y] = True
+                self.dfs((x, y), vis, n, m)
+
     def numIslands(self, grid):
         # code here
-        def f(i,j,vis):
-            for r,c in [[-1,0],[1,0],[0,1],[0,-1],[-1,1],[1,-1],[1,1],[-1,-1]]:
-                nr,nc = i+r,c+j
-                if 0<=nr<m and 0<=nc<n and (nr,nc) not in vis and grid[nr][nc] != 0:
-                    vis.add((nr,nc))
-                    f(nr,nc,vis)
-        cnt, vis = 0,set()
-        m, n = len(grid), len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] != 0 and (i,j) not in vis:
-                    cnt += 1
-                    vis.add((i,j))
-                    f(i,j,vis)
-        return cnt
+        n = len(grid)
+        m = len(grid[0])
+        vis = [[False] * m for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 'W':
+                    vis[i][j] = True
         
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if not vis[i][j]:
+                    count += 1
+                    self.dfs((i, j), vis, n, m)
+            
+        return count
+
+
 #{ 
  # Driver Code Starts
 # Driver code
 if __name__ == "__main__":
     for _ in range(int(input())):
-        n, m = map(int, input().strip().split())
-        grid = []
-        for i in range(n):
-            grid.append([int(i) for i in input().strip().split()])
+        n = int(input().strip())
+        m = int(input().strip())
+        grid = [input().strip().split() for _ in range(n)]
+
         obj = Solution()
         print(obj.numIslands(grid))
+        print("~")
 
 # } Driver Code Ends
