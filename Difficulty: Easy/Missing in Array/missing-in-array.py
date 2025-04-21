7f7040898b49e57f459db3b433fc9e5750a5286b
Rupesh Ravate <ravatez@gmail.com>
2025-04-21 22:41:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.py
+++ Difficulty: Easy/Missing in Array/missing-in-array.py
@@ -1,13 +1,12 @@
-#User function Template for python3
 class Solution:
-    
-    # Note that the size of the array is n-1
-    def missingNumber(self, n, arr):
-        import operator
-        from functools import reduce
-        return reduce(operator.xor, (i ^ a for i, a in enumerate(arr, 1)), n)
+    def missingNum(self, arr):
         # code here
-
+        arr.sort()
+        if(max(arr)==arr[-1] and len(arr)==max(arr)):
+            return arr[-1]+1
+        else:
+            n=max(arr)
+            return (n*(n+1)//2)-sum(arr)
 
 #{ 
  # Driver Code Starts
@@ -15,9 +14,9 @@ class Solution:
 
 t = int(input())
 for _ in range(0, t):
-    n = int(input())
     arr = list(map(int, input().split()))
-    s = Solution().missingNumber(n, arr)
+    s = Solution().missingNum(arr)
     print(s)
 
+    print("~")
 # } Driver Code Ends
