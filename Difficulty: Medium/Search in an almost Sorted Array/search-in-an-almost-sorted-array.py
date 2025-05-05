0e09e5566018ce44813c784f76801535dfa2550c
Rupesh Ravate <ravatez@gmail.com>
2025-05-05 19:51:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.py
@@ -0,0 +1,29 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+
+#User function Template for python3
+class Solution:
+    def findTarget(self, arr, target):
+        # code here
+        if target in arr:
+            return arr.index(target)
+        else:
+            return -1
+
+#{ 
+ # Driver Code Starts.
+
+if __name__ == "__main__":
+    t = int(input())  # Number of test cases
+
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))  # Read the array
+        target = int(input().strip())  # Read the target
+
+        sln = Solution()
+        ans = sln.findTarget(arr, target)
+        print(ans)
+        print("~")
+# } Driver Code Ends
