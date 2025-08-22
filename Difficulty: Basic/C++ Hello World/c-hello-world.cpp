cf2bf01245a3d2285c1ff5b4b31619691e034590
Rupesh Ravate <ravatez@gmail.com>
2025-08-22 11:00:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Hello World/c-hello-world.cpp
@@ -0,0 +1,11 @@
+
+
+class Solution {
+  public:
+    void helloWorld() {
+
+        // Type below cout<<"Hello World";
+        // cout will output to console
+        std::cout<<"Hello World";
+    }
+};
