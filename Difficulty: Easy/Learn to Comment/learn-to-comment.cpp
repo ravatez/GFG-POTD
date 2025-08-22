c76edb00db0b54efb11f56b8632f93d79d18346f
Rupesh Ravate <ravatez@gmail.com>
2025-08-22 13:27:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void printValues(int a, int b, int c) {
+        // code here
+        
+        cout << a << " ";
+        //cout << b << " ";
+        cout << c << " ";
+    }
+};
